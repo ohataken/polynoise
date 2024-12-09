9dc9c4a7adae14f3905eb16cab0737b895077264
ohataken <11092425+ohataken@users.noreply.github.com>
2024-12-09 22:12:34 +0900

    add Rectangle class

---
+++ src/browser/Rectangle.ts
@@ -0,0 +1,25 @@
+import Rectangular from './Rectangular';
+
+export default class Rectangle implements Rectangular {
+
+  readonly x: number;
+  readonly y: number;
+  readonly width: number;
+  readonly height: number;
+
+  constructor(x: number, y: number, width: number, height: number) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  getWidth(): number {
+    return this.width;
+  }
+
+  getHeight(): number {
+    return this.height;
+  }
+
+}
